83575bce716771f43b5f8f68d3b5a9b2e7d3c225
Csyhd <csyhd@msn.com>
2020-11-16 00:09:26 +0800

    feat:add delete function

--- app.js
+++ app.js
@@ -86,6 +86,14 @@ app.post('/restaurants/:id/edit', (req, res) => {
     .catch(error => console.log(error))
 })
 
+app.post('/restaurants/:id/delete', (req, res) => {
+  const id = req.params.id
+  return RestaurantList.findById(id)
+    .then(list => list.remove())
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
+
 app.listen(port, () => {
   console.log('start')
 })
