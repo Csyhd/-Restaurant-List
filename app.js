58f502ccc52242e6df12d86a3fb8d4cdf81847fc
Csyhd <csyhd@msn.com>
2020-11-15 23:16:41 +0800

    feat: project init and add seedr function

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const restaurantList = require('./restaurant.json')
+const mongoose = (require('mongoose'))
+const bodyParser = require('body-parser')
+const RestaurantList = require('./models/restaurantList')
 const app = express()
 const port = 3000
+const db = mongoose.connection
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: true }))
+
+mongoose.connect('mongodb://localhost/restaurant_2.0', { useNewUrlParser: true, useUnifiedTopology: true })
+
+db.on('error', () => {
+  console.log('mongodb error!!')
+})
+db.once('open', () => {
+  console.log('mongodb connected')
+})
 
 app.get('/', (req, res) => {
-
   res.render('index', { restaurants: restaurantList.results })
 })
 
